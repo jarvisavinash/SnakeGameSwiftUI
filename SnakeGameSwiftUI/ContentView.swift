87d483f194bd06a0ec227dd953c1047c00452dc5
Avinash Patil <jarvis.avinash@gmail.com>
2021-03-23 14:37:35 +0530

    Initial Commit

---
+++ SnakeGameSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnakeGameSwiftUI
+//
+//  Created by Avinash on 23/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnakeGameSwiftUI/SnakeGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameSwiftUIApp.swift
+//  SnakeGameSwiftUI
+//
+//  Created by Avinash on 23/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
